b37b6f233b3cf51917b7d6ca4f163405af33ed69
Frank Dye <otherwhitefrank@gmail.com>
2014-09-06 20:06:39 -0700

    Fixed rspec and basic relative linking.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,30 +5,43 @@ describe "StaticPages" do
   describe "Home Page" do
     
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
   describe "Help Page" do
   	
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+      visit help_path
   		expect(page).to have_content('Help')
   	end
 
   	it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -36,12 +49,12 @@ describe "StaticPages" do
   describe "About Us" do
 
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
+      visit about_path
   		expect(page).to have_content('About Us')
   	end
 
   	it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
@@ -49,15 +62,13 @@ describe "StaticPages" do
   describe "Contact Us" do
 
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact Us')
     end
 
     it "should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
     end
   end
-
-
 end
